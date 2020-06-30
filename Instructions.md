e11a181f58ff23f506134db90f7f2cb2b33d48d1
John Luo <jt.luo@mail.utoronto.ca>
2020-06-30 10:28:52 -0700

    Instructions for testing RBAC support for Linux in .NET 5 preview 6 (#1)

---
+++ Instructions.md
@@ -0,0 +1,27 @@
+# Instructions for testing RBAC support for Linux in .NET 5 preview 6
+
+1. Install .NET 5 preview 6 SDK
+   1. Instructions at https://dotnet.microsoft.com/download/dotnet/5.0.
+2. Domain join your linux server
+   1. Follow instructions at https://docs.citrix.com/en-us/linux-virtual-delivery-agent/current-release/installation-overview/ubuntu.html
+   2. Use Samba Windbind for step 3
+3. Modify your project
+   1. Update the TFM of your project to `net5.0`
+   2. Add the following packages to your project
+```xml
+<PackageReference Include="System.DirectoryServices.Protocols" Version="5.0.0-preview.6.20305.6" />
+<PackageReference Include="Microsoft.AspNetCore.Authentication.Negotiate" Version="5.0.0-preview.6.20312.15" />
+```
+   3. Add the file [LinuxAdapter.cs](https://github.com/JunTaoLuo/LinuxRBAC/blob/master/LinuxAdapter.cs) to your project
+   4. Modify the `ConfigureServices` method to the following:
+```C#
+var adapter = new LinuxAdapter("DOMAIN.net");
+services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate(options => options.Events = new NegotiateEvents()
+    {
+        OnAuthenticated = context => adapter.OnAuthenticated(context)
+    });
+```
+   1. Run the project and verify roles are populated via `User.IsInRole("<Role>")`.
+
+Note: the API shown here is not the final design. We plan to make this a built-in feature for the Negotiate middleware with options for configuration via `NegotiateOptions`.
---
+++ LinuxAdapter.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Authentication.Negotiate
+{
+    public class LinuxAdapter
+    {
+        private readonly string _distinguishedName;
+        private readonly LdapConnection _connection;
+
+        // example: domain = "DOMAIN.com"
+        public LinuxAdapter(string domain)
+        {
+            if (string.IsNullOrEmpty(domain))
+            {
+                throw new ArgumentNullException(nameof(domain));
+            }
+
+            _distinguishedName = domain.Split('.').Select(name => $"dc={name}").Aggregate((a, b) => $"{a},{b}");
+
+            LdapDirectoryIdentifier di = new LdapDirectoryIdentifier(server: domain, fullyQualifiedDnsHostName: true, connectionless: false);
+            _connection = new LdapConnection(di);
+            _connection.SessionOptions.ProtocolVersion = 3; //Setting LDAP Protocol to latest version
+            _connection.Timeout = TimeSpan.FromMinutes(1);
+            _connection.Bind(); // This line actually makes the connection.
+        }
+
+        public Task OnAuthenticated(AuthenticatedContext context, bool resolveNestedGroups = true)
+        {
+            var loggerFactory = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("LinuxAdapter");
+
+            var user = context.Principal.Identity.Name;
+            var userAccountName = user.Substring(0, user.IndexOf('@'));
+
+            string filter = $"(&(objectClass=user)(sAMAccountName={userAccountName}))"; // This is using ldap search query language, it is looking on the server for someUser
+            SearchRequest searchRequest = new SearchRequest(_distinguishedName, filter, SearchScope.Subtree, null);
+            SearchResponse searchResponse = (SearchResponse)_connection.SendRequest(searchRequest);
+
+            if (searchResponse.Entries.Count > 0)
+            {
+                if (searchResponse.Entries.Count > 1)
+                {
+                    logger.LogWarning($"More than one response received for query: {filter} with distinguished name: {_distinguishedName}");
+                }
+
+                var userFound = searchResponse.Entries[0]; //Get the object that was found on ldap
+                var memberof = userFound.Attributes["memberof"]; // You can access ldap Attributes with Attributes property
+
+                var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+
+                foreach (var group in memberof)
+                {
+                    // Example distinguished name: CN=TestGroup,DC=KERB,DC=local
+                    var groupDN = $"{Encoding.UTF8.GetString((byte[])group)}";
+                    var groupCN = groupDN.Split(',')[0].Substring("CN=".Length);
+
+                    if (resolveNestedGroups)
+                    {
+                        GetNestedGroups(claimsIdentity, groupCN, logger);
+                    }
+                    else
+                    {
+                        AddRole(claimsIdentity, groupCN);
+                    }
+                }
+            }
+            else
+            {
+                logger.LogWarning($"No response received for query: {filter} with distinguished name: {_distinguishedName}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void GetNestedGroups(ClaimsIdentity principal, string groupCN, ILogger logger)
+        {
+            string filter = $"(&(objectClass=group)(sAMAccountName={groupCN}))"; // This is using ldap search query language, it is looking on the server for someUser
+            SearchRequest searchRequest = new SearchRequest(_distinguishedName, filter, System.DirectoryServices.Protocols.SearchScope.Subtree, null);
+            SearchResponse searchResponse = (SearchResponse)_connection.SendRequest(searchRequest);
+
+            if (searchResponse.Entries.Count > 0)
+            {
+                if (searchResponse.Entries.Count > 1)
+                {
+                    logger.LogWarning($"More than one response received for query: {filter} with distinguished name: {_distinguishedName}");
+                }
+
+                var group = searchResponse.Entries[0]; //Get the object that was found on ldap
+                string name = group.DistinguishedName;
+                AddRole(principal, name);
+
+                var memberof = group.Attributes["memberof"]; // You can access ldap Attributes with Attributes property
+                if (memberof != null)
+                {
+                    foreach (var member in memberof)
+                    {
+                        var groupDN = $"{Encoding.UTF8.GetString((byte[])member)}";
+                        var nestedGroupCN = groupDN.Split(',')[0].Substring("CN=".Length);
+                        GetNestedGroups(principal, nestedGroupCN, logger);
+                    }
+                }
+            }
+        }
+
+        private static void AddRole(ClaimsIdentity identity, string role)
+        {
+            identity.AddClaim(new Claim(identity.RoleClaimType, role));
+        }
+    }
+}
